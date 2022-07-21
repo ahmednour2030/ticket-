7d2126bf7d0be0bda02a2ffe6a000fbfa7004c86
ahmednour2030 <a_mohamednour@yahoo.com>
2022-07-21 18:31:17 +0200

    update

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\InvoiceController;
+use App\Http\Controllers\Api\TicketController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -25,6 +26,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::group([ 'middleware' => 'api'], function (){
+    Route::get('tickets', [TicketController::class, 'index']);
     Route::post('invoice', [InvoiceController::class, 'store']);
     Route::post('invoice/entry', [InvoiceController::class, 'entry']);
 });
